337bf1d87acec7b28acb81ea01cde087ad9ac0ec
aleev_b <b.aleev@krlb.ru>
2020-04-14 10:37:22 +0300

    modified tryAdvance method

--- src/main/java/PersonSpliterator.java
+++ src/main/java/PersonSpliterator.java
@@ -5,14 +5,26 @@ import java.util.function.Consumer;
 
 public class PersonSpliterator implements Spliterator<Person> {
     private final Spliterator<String> lineSpliterator;
+    private String name;
+    private int age;
+    private String city;
 
     public PersonSpliterator(Spliterator<String> lineSpliterator){
         this.lineSpliterator = lineSpliterator;
     }
 
     @Override
-    public boolean tryAdvance(Consumer action) {
-        return false;
+    public boolean tryAdvance(Consumer<? super Person> action) {
+        if (this.lineSpliterator.tryAdvance(line-> this.name = line) &&
+        this.lineSpliterator.tryAdvance(line-> this.age = Integer.parseInt(line)) &&
+        this.lineSpliterator.tryAdvance(line-> this.city = line)){
+            Person p = new Person (name, age, city);
+            action.accept(p);
+            return true;
+        } else{
+            return false;
+        }
+
     }
 
     @Override
